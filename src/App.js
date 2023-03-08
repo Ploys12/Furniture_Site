44b09f6d69f55d0521274c65e12fd22da212a8c4
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-03-09 00:40:31 +0400

    Добавлено состояние с товарами и их описанием. Добавлены компоненты Items и Item для вывода товаров на странице и также стили к ним.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import Header from './Copmonents/Header'
 import Footer from './Copmonents/Footer'
-function App(){
-    return (<div className='wrapper'><Header /> <Footer /></div>)
+import Items from './Copmonents/Items'
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            items: [
+                {id: 1, title: "Стул серый", img: "grey_chair.jpg", desc: "Серый стул", category: "chairs", price: "49.99"},
+             {id: 2, title: "Стол", img: "table.jpg", desc: "Стол", category: "tables", price: "149.00"},
+              {id: 3, title: "Шкаф", img: "wardrobe.jpg", desc: "Шкаф", category: "wardrobes", price: "100.00"},
+               {id: 4, title: "Плита", img: "stove.jpg", desc: "Плита кухонная", category: "stoves", price: "120.00"},
+                {id: 5, title: "Холодильник", img: "fridge.jpg", desc: "Холодильник", category: "fridges", price: "80.00"}
+            ]
+        }
+    }
+
+
+    render(){
+    return (<div className='wrapper'>
+        <Header />
+        <Items items={this.state.items}/>
+         <Footer />
+         </div>)}
 }
 export default App
