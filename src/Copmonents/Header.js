3c4e783e09e547d5ebecb31b078f5d6bf081bac0
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-03-14 13:29:02 +0400

    Если товаров в корзине нет, то выводится надпись 'товаров нет'

--- src/Copmonents/Header.js
+++ src/Copmonents/Header.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react'
 import { FaShoppingBasket } from "react-icons/fa"
 import Order from './Order'
 
+
+function showOrders(props){
+  return(<div>{props.orders.map(el => (<Order key={el.id} item={el}/>))}</div>)
+
+
+}
+
+function showNothing(){
+  return (<div className='empty'><h2>Товаров нет</h2></div>)
+}
+
 export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false)
 
@@ -18,7 +29,7 @@ export default function Header(props) {
             <FaShoppingBasket onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
             {cartOpen && (
               <div className='shop-cart'>
-                {props.orders.map(el => (<Order key={el.id} item={el}/>))}
+                {props.orders.length > 0 ? showOrders(props) : showNothing()}
               </div>
             )}
         </div>
